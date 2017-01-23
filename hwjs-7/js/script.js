d5e31a986c3cd2e3975c5c1499230560b4d8aa0b
Gaidai Andrii <gaydayav@gmail.com>
2017-01-23 02:14:41 +0200

    Initial and seemed finnal commit.

---
+++ hwjs-7/js/script.js
@@ -0,0 +1,98 @@
+(function () {
+'use strict';
+})();
+
+$(function () {
+  
+  // TABS =========================================================
+
+  var $links = $('.tab-link');
+  var $tabs = $('.tab');
+
+  pageInit($links, $tabs);
+
+  $links.focus(function () {
+    var currentLink = $(this),
+        currentTab = currentLink.attr('href');
+
+    toggleActiveLink($links, currentLink);
+    toggleTabs($tabs, currentTab);
+  });
+
+  function pageInit(links, tabs) {
+    toggleActiveLink(links, links.first());
+    toggleTabs(tabs, tabs.first());
+  }
+
+  function toggleActiveLink(links, current) {
+    links.removeClass('tab-active');
+    current.addClass('tab-active');
+  }
+
+  function toggleTabs(tabs, tabId) {
+    tabs.hide();
+    $(tabId).show();
+  }
+
+  // FORM =========================================================
+
+  var $inputs = $('.form__input');
+  var $formBtn = $('.button');
+
+  var $tooltips = createTooltips($inputs);
+
+  $inputs.hover(function() {
+    showTooltips($(this).next());
+  }, function () {
+    hideTooltips($(this).next());
+  });
+
+  $formBtn.click(function() {
+    showTooltips($tooltips);
+  });
+
+  function showTooltips(list) {
+    $(list).each(function() {
+      $(this).animate({
+        opacity: 'show',
+        left: '260'},
+        300, function() {
+        /* stuff to do after animation is complete */
+      });
+    });
+  }
+
+  function hideTooltips(list) {
+    $(list).each(function() {
+      $(this).animate({
+        opacity: 'hide',
+        left: '270'},
+        150, function() {
+        /* stuff to do after animation is complete */
+      });
+    });
+  }
+
+  function createTooltips(inputs) {
+    var tooltipsList = [];
+
+    inputs.each(function() {
+      var $tooltip = $(document.createElement('span'));
+      var parentLabel = $(this).parent();
+      var tooltipText = $(this).attr('title');
+
+      $tooltip.addClass('form__tooltip');
+      $tooltip.text(tooltipText);
+
+      $tooltip.appendTo(parentLabel);
+      $tooltip.hide();
+
+      tooltipsList.push($tooltip);
+
+      $(this).removeAttr('title');
+    });
+
+    return tooltipsList;
+  }
+
+});
