f3cee9d94b257d09a3ad2d164ccf4f13f60d55a5
Gaidai Andrii <gaydayav@gmail.com>
2017-03-05 18:37:46 +0200

    HWjs-16. Initial empty commit.

---
+++ hwjs-15/src/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, maximum-scale=1, initial-scale=1, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="favicon.ico">
+    <title>AJAX</title>
+</head>
+<body>
+    <input id="show_ip" type="button" value="Показать мой IP">
+    <div id="myip"></div>
+    
+    <form action="">
+      Email:<br>
+      <input type="email" name="email"><br>
+      Телефон:<br>
+      <input type="phone" name="phone"><br>
+      Имя:<br>
+      <input type="text" name="name"><br>
+      <input type="button" id="send" value="Отправить">
+    </form>
+    <div id="result"></div>
+
+    <script src="js/common.js"></script>
+</body>
+</html>
---
+++ hwjs-15/src/js/common.js
@@ -0,0 +1,85 @@
+window.onload = function(){
+  var inp_email = document.querySelector('input[name=email]');
+  var inp_phone = document.querySelector('input[name=phone]');
+  var inp_name = document.querySelector('input[name=name]');
+
+  document.querySelector('#send').onclick = function(){
+    var params = 'email=' + inp_email.value + '&' + 
+                 'phone=' + inp_phone.value + '&' + 
+                 'name=' + inp_name.value;
+    ajaxPost('a=b');
+  };
+};
+
+// window.onload = function () {
+//   document.querySelector('#show_ip').onclick = function() {
+//     ajaxGet('ip.php', function(data){
+//       console.log(data);
+//     });
+
+//     ajaxGet('ip.php?id=12&a=b&hz=nz', function (data) {
+//       document.querySelector('#myip').innerHTML = data;
+//     });
+//   };
+// };
+
+// function ajaxGet(url, callback){
+//   var f = callback || function(data){};
+
+//   var request = new XMLHttpRequest();
+
+  // request.readyState // 0 - new
+  // request.readyState // 1 - open
+  // request.readyState // 2 - send
+  // request.readyState // 3 - частично пришел ответ
+  // request.readyState // 4 - ответ пришел полнотсью
+
+
+//   request.onreadystatechange = function() {
+//     if (request.readyState == 4 && request.status == 200) {
+//       f(request.responseText);
+//     }
+//   };
+
+//   request.open('GET', url);
+//   request.send();
+// }
+
+
+function ajaxPost(params){
+  var request = new XMLHttpRequest();
+
+  request.onreadystatechange = function() {
+    if (request.readyState == 4 && request.status == 200) {
+      if (request.responseText == '1') {
+        document.querySelector('#result').innerHTML = 'Ура! Все хорошо!';
+        document.querySelector('form').style.display = 'none';
+      } else{
+        document.querySelector('#result').innerHTML = request.responseText;
+      }
+    }
+  };
+
+  request.open('POST', 'app.php');
+  request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  // request.send(params);
+}
+
+function getXmlHttp(){
+  var xmlhttp;
+  try {
+    xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+  } catch (e) {
+    try {
+      xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    } catch (E) {
+      xmlhttp = false;
+    }
+  }
+  if (!xmlhttp && typeof XMLHttpRequest!='undefined') {
+    xmlhttp = new XMLHttpRequest();
+  }
+  return xmlhttp;
+}
+
+
