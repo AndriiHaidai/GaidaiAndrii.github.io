429a7fda0c61c4b47667b99a9976e28826419b07
Gaidai Andrii <gaydayav@gmail.com>
2017-03-20 14:11:33 +0200

    Exam. Refactored gulpfile, Susy, globals structure.

--- exam/gulpfile.js
+++ exam/gulpfile.js
@@ -10,24 +10,36 @@ const cssnano      = require('gulp-cssnano');
 const imagemin     = require('gulp-imagemin');
 const plugins      = require('gulp-load-plugins')();
 const sass         = require('gulp-sass');
-// const susy         = require('susy');
+
 
 const source = {
   html: './src/index.html',
-  sass: ['./src/blocks/tools/*.scss', './src/blocks/default/*.scss', './src/blocks/**/*.scss'],
-  js: './src/blocks/**/*.js'
+  sass: ['./src/blocks/globals/*.scss', './src/blocks/**/*.scss'],
+  js: './src/blocks/**/*.js',
+  img: './src/img/**/*.{jpg,png}',
+  svgIco: './scr/img/**/*.svg'
 };
 
-const destination = {
-  folder: './src',
-  css: './src/css',
-  js: './src/js',
+const destin = {
+  html: './dest',
+  css: './dest/css',
+  js: './dest/js',
+  img: './dest/img',
+  svgSprite: './dest/sprite'
 };
 
-// Assembling .scss files
+// HTML
+gulp.task('rigger', function() {
+  gulp.src(source.html)
+  .pipe(plugins.rigger())
+  .pipe(gulp.dest(destin.html))
+  .pipe(browserSync.reload( {stream: true} ));
+});
+
+// Styles
 gulp.task('styles' , function(){
   return gulp.src(source.sass)
-  .pipe(plugins.sourcemaps.init())
+  // .pipe(plugins.sourcemaps.init())
   .pipe(plugins.sassGlobImport())
   .pipe(plugins.sass({
     outputStyle: 'expanded', 
@@ -35,34 +47,47 @@ gulp.task('styles' , function(){
   }).on('error', plugins.sass.logError))
   .pipe(plugins.concat('style.css'))
   .pipe(autoprefixer({browsers: ['last 5 versions', 'IE 9'], cascade: true }))
-  .pipe(plugins.sourcemaps.write())
-  .pipe(gulp.dest(destination.css))
+  // .pipe(plugins.sourcemaps.write())
+  .pipe(gulp.dest(destin.css))
   .pipe(browserSync.reload( {stream: true} ));
 });
 
-// Assembling .js files
+// Javascript
 gulp.task('bundleJs', function() {
   gulp.src(source.js)
   .pipe(plugins.concat('common.min.js'))
-  .pipe(gulp.dest(destination.js))
+  .pipe(gulp.dest(destin.js))
   .pipe(browserSync.reload( {stream: true} ));
 });
 
 
+// Images
+gulp.task('imgmin', function () {
+  gulp.src(source.img)
+  .pipe(imagemin({}))
+  .pipe(gulp.dest(destin.img))
+  .pipe(browserSync.reload({stream: true }));
+});
+
+
+// Watch
 gulp.task('watch', function(){
-  gulp.watch(source.html, browserSync.reload);
+  gulp.watch(destin.html, browserSync.reload);
+  gulp.watch(source.html, ['rigger'], browserSync.reload);
   gulp.watch(source.sass, {cwd: './'}, ['styles']);
+  gulp.watch(destin.img, {cwd: './'}, ['imgmin']);
   gulp.watch(source.js, {cwd: './'}, ['bundleJs']);
 });
+
 
 gulp.task('browser-sync', function(){
   browserSync({
     server: {
-      baseDir: 'src'
+      baseDir: './dest'
     },
     notify: false
   });
 });
 
 
-gulp.task('default', ['browser-sync', 'styles', 'bundleJs', 'watch']);
+gulp.task('default', ['browser-sync', 'rigger', 'styles', 'imgmin', 'bundleJs', 'watch']);
