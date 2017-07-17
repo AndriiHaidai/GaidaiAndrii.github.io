ce47eb499a7d25fe4cae583260d0f04a7e579fcf
Gaidai Andrii <gaydayav@gmail.com>
2017-07-17 19:15:44 +0300

    Portfolio PSD to HTML. Transition from header section to About section is done. Section About is started.

--- portfolio/src/blocks/about.js
+++ portfolio/src/blocks/about.js
@@ -28,13 +28,3 @@
     // event.preventDefault(event);
 
 }(jQuery, undefined));
-//Include 'use strict'; as the first statement in a wrapping function, so it only affects that function. This prevents problems when concatenating scripts that aren't strict.
-
-(function () {
-   'use strict';
-   // this function is strict...
-}());
-
-(function () {
-   // but this function is sloppy...
-}());
