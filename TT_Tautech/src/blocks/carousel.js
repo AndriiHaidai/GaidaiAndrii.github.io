4b61d461dfa434a823f544211caec66c51abfd1e
Gaidai Andrii <gaydayav@gmail.com>
2017-07-19 22:43:25 +0300

    TauTech test task. Gallery is done responsive.

--- TT_Tautech/src/blocks/carousel.js
+++ TT_Tautech/src/blocks/carousel.js
@@ -182,38 +182,3 @@
 
 //   return a;
 // }
-
-(function () {
-  'use strict';
-
-  var menuWide = document.querySelector('.menu__wide');
-  var menuIcon = document.querySelector('.menu__icon');
-
-  menuIcon.addEventListener('click', menuShowHide);
-  
-  document.addEventListener('keydown', function(e) {
-    if (e.code == 'KeyM') {
-      menuShowHide();
-    }
-    if (e.code == 'Escape') {
-      menuHide();
-    }
-  });
-
-  function menuShow(){
-    if (!menuWide.classList.contains('show')) {
-      menuShowHide();
-    }
-  }
-
-  function menuHide(){
-    if (menuWide.classList.contains('show')) {
-      menuShowHide();
-    } 
-  }
-
-  function menuShowHide() {
-    menuWide.classList.toggle('show');
-  }
-
-}());
