586a8e7c555c4ac3fd60d4b351e9cb56b1bca915
Gaidai Andrii <gaydayav@gmail.com>
2017-08-04 12:12:28 +0300

    TauTech test task. Two Callouts are done.

--- TT_Tautech2/src/blocks/canvas.js
+++ TT_Tautech2/src/blocks/canvas.js
@@ -10,8 +10,8 @@
 // })();
 
 // Variables:
-var canvas;
-var ctx;
+const canvas = document.getElementById('c1');
+const ctx = canvas.getContext('2d');
 var ctxWdth;
 var ctxHght;
 
@@ -28,14 +28,25 @@ var YY; // конечная координата отрисовки график
 
 var grd;
 
-(function initCanvas(){
+var points = [];
+var point = 1;
+// var nextTime = new Date().getTime() + 500;
+// var pace = 200;
+// var speed = 2;
+var tgtX = 0;
+var tgtY = 0;
+var x = 0;
+var y = 0;
+
+
+function initCanvas(){
   ctxWdth = 1000;
   ctxHght = 700;
 
-  canvas = document.getElementById('c1');
+  // canvas = document.getElementById('c1');
   canvas.width = ctxWdth;
   canvas.height = ctxHght;
-  ctx = canvas.getContext('2d');
+  // ctx = canvas.getContext('2d');
   
   ctxPdngBegX = 100;  // Отступ области отрисовки внутри Canvas слева
   ctxPdngEndX = 100;  // Отступ области отрисовки внутри Canvas справа
@@ -48,15 +59,17 @@ var grd;
   XX = ctxWdth - ctxPdngBegX - ctxPdngEndX; // конечная координата отрисовки графика внутри Canvas
   YY = ctxHght - ctxPdngBegY - ctxPdngEndY; // конечная координата отрисовки графика внутри Canvas
 
+  ctx.setTransform(1,0,0,1,0,0);
+  ctx.clearRect(0, 0, ctxWdth, ctxHght);
   ctx.translate(0, ctxHght);
   ctx.scale(1, -1);
   ctx.translate(ctxPdngBegX, ctxPdngEndY);
-})();
+}
 
-(function drawGrid(){
+function drawGrid(){
   // Фон под отрисовку: 2 строки:
-  ctx.fillStyle = '#ccc';
-  ctx.fillRect(0,0, XX,YY);
+  // ctx.fillStyle = '#18223B';
+  // ctx.fillRect(0,0, XX,YY);
   
   // Сама сетка:
   ctx.strokeStyle = "#424A5E";
@@ -73,136 +86,10 @@ var grd;
   grd.addColorStop(1, '#0296E9');
   ctx.strokeStyle = grd;
   ctx.font = '20px sans-serif';
-})();
-
-  // ctx.fillStyle = grd;
-// ctx.fillStyle = "#fff";
-// ctx.strokeStyle = "#fff";
-// ctx.lineWidth = "2";
-
-// ctx.fillRect(10, 10, 90, 90);
-// ctx.clearRect(30, 30, 50, 50);
-// ctx.strokeRect(35, 35, 40, 40);
-
-// function rad(deg) {return (Math.PI / 180) * deg; }
-// function roundedRect(ctx,x,y,width,height,radius){
-//   ctx.beginPath();
-//   ctx.moveTo(x,y+radius);
-//   ctx.lineTo(x,y+height-radius);
-//   ctx.quadraticCurveTo(x,y+height,x+radius,y+height);
-//   ctx.lineTo(x+width-radius,y+height);
-//   ctx.quadraticCurveTo(x+width,y+height,x+width,y+height-radius);
-//   ctx.lineTo(x+width,y+radius);
-//   ctx.quadraticCurveTo(x+width,y,x+width-radius,y);
-//   ctx.lineTo(x+radius,y);
-//   ctx.quadraticCurveTo(x,y,x,y+radius);
-//   ctx.stroke();
-// }
-
-// ctx.beginPath();
-// ctx.moveTo(20, 20);
-// ctx.lineTo(40, 40);
-// ctx.lineTo(60, 50);
-// ctx.lineTo(80, 80);
-// ctx.lineTo(100, 70);
-// ctx.lineTo(120, 110);
-// ctx.lineTo(140, 120);
-// ctx.lineTo(160, 130);
-// ctx.lineTo(180, 150);
-// ctx.rect(190, 170, 20, 20);
-// ctx.lineTo(150, 220);
-// ctx.stroke();
-// ctx.closePath();
-
-// ctx.beginPath();
-// // ctx.moveTo(0, 0);
-// // ctx.lineTo(10, 100);
-// // ctx.stroke();
-// ctx.rect(10, 10, 5, 5);
-// // ctx.moveTo(0, 0);
-// ctx.lineTo(5, 5);
-// ctx.stroke();
-
-// roundedRect(ctx, 30, 150, 30, 30, 5);
-
-// ctx.beginPath();
-// ctx.rect(40, 200, 40, 40);
-// ctx.fill('nonzero');
-
-
-var points = [];
-var point = 1;
-var nextTime = new Date().getTime() + 500;
-var pace = 200;
-var speed = 2;
-var tgtX = 0;
-var tgtY = 0;
-var x = 0;
-var y = 0;
-
-
-// // Make some points:
-// for (var i = 0; i < 90; i++) {
-//   points.push({
-//     x: i * (XX/90),
-//     y: 100 + Math.sin(i) * 50
-//   });
-// }
-
-// x = points[0].x;
-// y = points[0].y;
-// tgtX = points[1].x;
-// tgtY = points[1].y;
-
-// function draw() {
-//   if ( new Date().getTime() > nextTime ) {
-//     nextTime = new Date().getTime() + pace;
-//     point++;
-//     if ( point > points.length ) {
-//       point = 0;
-//     }
-//   }
-  
-//   ctx.beginPath();
-//   ctx.lineWidth = "2";
-//   ctx.strokeStyle = "#2068A8";
-//   ctx.fillStyle = "#2068A8";
-
-//   // ctx.moveTo(points[0].x, points[0].y);
-//   // for (var p = 1, plen = point; p < plen; p++ ) {
-//   //   ctx.lineTo(points[p].x, points[p].y);
-//   //   console.log(points[0].x, points[0].y);
-//   // }
-  
-//   ctx.moveTo(points[0].x, points[0].y);
-//   for (var p = 1; p < points.length -2 ; p++ ) {
-//     var xc = (points[p].x + points[p + 1].x) / 2;
-//     var yc = (points[p].y + points[p + 1].y) / 2;
-//     ctx.quadraticCurveTo(points[p].x, points[p].y, xc, yc);
-//     ctx.fillRect(points[p].x, points[p].y, 4, 4);
-//     // console.log(points[0].x, points[0].y);
-//     // ctx.lineTo(points[p].x, points[p].y);
-//   }
-  
-//   ctx.stroke();
-
-//   requestAnimationFrame(draw);
-// }
-
-// draw();
-
-
-// Make some points:
-var qtyPoints = 90;
-for (var i = 0; i <= qtyPoints; i++) {
-  points.push(i * (XX/qtyPoints));
-  points.push(40 + 3*Math.sin(i) + 4*i + 5*Math.random() );
 }
 
-drawLabels();
 function drawLabels() {
   // y labels:
-    // ctx.textAlign('left');
     ctx.font = 'normal normal 100 14px sans-serif';
     ctx.fillStyle = '#fff';
     ctx.textAlign = 'center';
@@ -234,22 +121,24 @@ function drawLabels() {
     ctx.scale(1, -1);
 }
 
-
-var tension = 0.5;
-
+var tension = 0.0;
 function drawCurve(ctx, ptsa, tension, isClosed, numOfSegments, showPoints) {
 
+    // debugger;
+    console.log('ctx: ', ctx);
     showPoints  = showPoints ? showPoints : false;
 
     ctx.beginPath();
 
     drawLines(ctx, getCurvePoints(ptsa, tension, isClosed, numOfSegments));
 
+    ctx.stroke();
     if (showPoints) {
-        ctx.stroke();
-        ctx.beginPath();
-        for(var i=0;i<ptsa.length-1;i+=2) 
-                ctx.rect(ptsa[i] - 2, ptsa[i+1] - 2, 4, 4);
+      ctx.beginPath();
+      for(var i = 0; i < ptsa.length - 1; i += 2) {
+        ctx.fillRect(ptsa[i] - 2, ptsa[i+1] - 2, 4, 4);
+      }
+      ctx.closePath();
     }
 }
 
@@ -331,21 +220,53 @@ function drawLines(ctx, pts) {
     for(i=2;i<pts.length-1;i+=2) ctx.lineTo(pts[i], pts[i+1]);
 }
 
-drawCurve(ctx, points, tension, false, false, true);
+function drawVertical(x_) {
+  const y1 = -40;       // Отступ первой выноски вниз от области рисования графика.
+  const widthVert = 3; // Толщина вертикальной линии с выносками.
+  var x = x_;
+  var y2; // Высота второй выноски - на графике.
 
-function drawVertical(x, y, w) {
+  // Найти ближайшую правую точку x:
+  // Формат массива: [x0,y0, x1,y1, ... xN,yN]
+  var left = 0;
+  var middle;
+  var right = qtyPoints*2;
+  console.log('points[0]: ', points[0]);
+  console.log('points[qtyPoints*2]: ', points[qtyPoints*2]);
+  console.log('points: ', points);
+
   ctx.fillStyle = '#4A90E2';
-  ctx.fillRect(x, y, w, YY+50);
 
-  drawCallout(x+w/2, y, w, 20,8,4, 'Text1 goes here.', 'texygt2', '11px Arial', false);
+  while (left <= right) {
+    middle = Math.ceil((left+right)/2);
+    middle = Math.ceil(middle/2)*2; // Делаем число четным "вправо".
+    if ( points[middle] === x ) {
+      break; // Остановиться с результатом x.
+    } else if ( points[middle] > x ) {
+      right = middle - 2;
+    } else if ( points[middle] < x ) {
+      left = middle + 2;
+    }
+  }
+  
+  var k;
+  if ( Math.abs(x - points[0]) < Math.abs(x - points[2]) ) {
+    k = 0; // Костыль для отрисовки нулевой точки.
+  } else {
+    k = Math.max(left, middle); // Отрисовка ближайшей правой точки.
+  }
+  x = points[k];
+  y2 = points[k+1];
+
+  ctx.fillRect(x - widthVert/2, y1, widthVert, YY-y1);
+  drawCallout(x, y1, widthVert, x, 'texygt2', '12px Arial', '#4A90E2', '#18223B', 20,8,4, false);
+  drawCallout(x, y2, widthVert, y2, '', '12px Arial', '#3CC182', '#3CC182', 20,8,4, true);
 
 }
 
-drawVertical(150, -50, 3);
-
-function drawCallout(x, y, widthVertical, R_, r_, r2_, text1, text2, font, toTop_) {
-  ctx.fillStyle = "#fff";
-  ctx.strokeStyle = '#4A90E2';
+function drawCallout(x, y, rBullet, text1, text2, font, strokeStyle, fillStyle, R_, r_, r2_, toTop_) {
+  ctx.strokeStyle = strokeStyle;
+  ctx.fillStyle = fillStyle;
   ctx.lineWidth = '1';
   ctx.font = font; // Первым должен идти размер шрифта!
 
@@ -357,20 +278,12 @@ function drawCallout(x, y, widthVertical, R_, r_, r2_, text1, text2, font, toTop
   var fz = parseInt(font); // font size
   var lh = 1.4;            // line height
   // Callout box Width and Height:
-  var W = r*2 + Math.max(ctx.measureText(text1).width, ctx.measureText(text2).width);
   var H = r + r2 + fz + ( text2 ? fz*lh : 0 );
-
-  ctx.scale(1, -1);
-  ctx.textBaseline = 'middle';
-  ctx.textAlign = 'center';
-  ctx.fillText(text1, x, ( -y + R + r + fz/2 ), W);
-  if (text2) {ctx.fillText(text2, x, ( -y + R + r + fz/2 + fz*lh ), W);}
-  ctx.scale(1, -1);
+  var W = r*2 + Math.max(ctx.measureText(text1).width, ctx.measureText(text2).width);
+      W = Math.max(W, H*2, 80);
 
   var w = W/2 - R - r;
   var h = H - r - r2;
-
-  var rBullet = widthVertical;
   
   var xc, yc; // x.control -  Координаты контрольной точки для дуги;
   var xe, ye; // x.end - Координаты "последней" точки;
@@ -439,13 +352,53 @@ function drawCallout(x, y, widthVertical, R_, r_, r2_, text1, text2, font, toTop
   ctx.arcTo(xc, yc, xe, ye, R, true);
 
   ctx.stroke();
+  ctx.fill();
+
+  // Оси перевернуты, чтобы рисовать графики правильно, поэтому текст нужно отзеркалить:
+  ctx.scale(1, -1);
+  ctx.textBaseline = 'middle';
+  ctx.textAlign = 'center';
+  ctx.fillStyle = "#fff";
+  ctx.fillText(text1, x, -y + (R + r + fz/2 )*toTop, W);
+  if (text2) {ctx.fillText(text2, x, -y + (R + r + fz/2 + fz*lh )*toTop, W);}
+  ctx.scale(1, -1);
 
 
   // Соединительный кружок на конце выноски;
   ctx.beginPath();
   ctx.moveTo(x + rBullet, y - rBullet*0.5);
   ctx.arc(x, y - rBullet*0.5, rBullet, 0, 2*Math.PI);
+  ctx.fillStyle = "#fff";
   ctx.fill();
   ctx.lineWidth = '1';
   ctx.stroke();
 }
+
+initCanvas();
+
+// Make some points:
+// Формат массива: [x0,y0, x1,y1, ... xN,yN]
+var qtyPoints = 90;
+for (var i = 0; i <= qtyPoints; i++) {
+  points.push(i * (XX/qtyPoints));
+  points.push(40 + 3*Math.sin(i) + 4*i + 5*Math.random() );
+}
+
+drawGrid();
+drawLabels();
+drawCurve(ctx, points, tension, false, false, false);
+drawVertical(0);
+
+canvas.addEventListener('mousemove', function (e) {
+  var mouseX = e.clientX - canvas.getBoundingClientRect().left - ctxPdngBegX;
+  var mouseY = e.clientY - canvas.getBoundingClientRect().top  - ctxPdngBegY;
+  if ( 0 < mouseX && mouseX < XX && 0 < mouseY && mouseY < YY ) {
+    // console.log('mouseX: ', mouseX, '; mouseY: ', mouseY, '; ctx: ', ctx);
+
+    initCanvas();
+    drawLabels();
+    drawGrid();
+    drawCurve(ctx, points, tension, false, false, false);
+    drawVertical(mouseX);
+  }
+});
