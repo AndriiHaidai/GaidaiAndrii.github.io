e653ab1367e1345d1211836a98563a54ee24f76c
Gaidai Andrii <gaydayav@gmail.com>
2017-08-11 02:36:35 +0300

    TauTech test task. Refactored WorkArray format.

--- TT_Tautech2/src/js/common.js
+++ TT_Tautech2/src/js/common.js
@@ -27,11 +27,8 @@ var YY; // конечная координата отрисовки график
 
 var grd;
 
-var pointsSrc = [];
-var xPoints = [];
-var yPoints = [];
-var pointsAGA = {x:[], y:[]};
-var pointsWork = {x:[], y:[]};
+var pointsSrc = {x:[], y:[]}; // pointsSrc = [];
+var pointsWork = {xVal:[], xLbl:[], yVal:[], yLbl:[]};
 var point = 1;
 // var nextTime = new Date().getTime() + 500;
 // var pace = 200;
@@ -56,12 +53,12 @@ function initCanvas(){
   ctxPdngBegY = 50;   // Отступ области отрисовки внутри Canvas сверху
   ctxPdngEndY = 150;  // Отступ области отрисовки внутри Canvas снизу
 
-  xx = ctxPdngBegX;           // начальная координата отрисовки графика внутри Canvas
-  yy = ctxPdngBegY;           // начальная координата отрисовки графика внутри Canvas
+  xx = ctxPdngBegX;                         // начальная координата отрисовки графика внутри Canvas
+  yy = ctxPdngBegY;                         // начальная координата отрисовки графика внутри Canvas
   XX = ctxWdth - ctxPdngBegX - ctxPdngEndX; // конечная координата отрисовки графика внутри Canvas
   YY = ctxHght - ctxPdngBegY - ctxPdngEndY; // конечная координата отрисовки графика внутри Canvas
 
-  ctx.setTransform(1,0,0,1,0,0);
+  ctx.setTransform(1,0,0,1,0,0); // Обнуляем трансформации.
   ctx.clearRect(0, 0, ctxWdth, ctxHght);
   ctx.translate(0, ctxHght);
   ctx.scale(1, -1);
@@ -69,40 +66,52 @@ function initCanvas(){
 }
 
 function getDataPoints(_qtyPoints, _hgt) {
-  var ar = [];
+  var ar = {x:[], y:[]};
   var hgtBeg = 50*Math.random(); // 30
-  var hgtEnd = _hgt ? _hgt : (100-hgtBeg)/_qtyPoints*Math.random(); //500
+  var hgtEnd = _hgt ? _hgt : (70-hgtBeg)/_qtyPoints*Math.random(); //500
+  var xPoints = [];
+  var yPoints = [];
 
   for (var i = 0; i <= _qtyPoints; i++) {
     xPoints.push(i * (XX/_qtyPoints));
     yPoints.push( Math.round( (20+hgtBeg + 3*Math.sin(i) + hgtEnd*i + 5*Math.random())*10000 )/10000 );
-    ar.push(xPoints[i]);
-    ar.push(yPoints[i]);
-    pointsAGA.x.push(xPoints[i]);
-    pointsAGA.y.push(yPoints[i]);
+    ar.x.push(xPoints[i]);
+    ar.y.push(yPoints[i]);
   }
   return ar;
 }
 
 var grid = {};
-function countGrid() {
+var gridScaled = {
+  Beg: 0,
+  End: 0,
+  stepY: 0,
+  xVal: [],
+  xLbl: [],
+  yVal: [],
+  yLbl: []
+};
+function countScaledGrid(ptsa, _accuracy) {
+  var accu = _accuracy ? _accuracy : 4;
+  var i;
   var ValMid;
   var ValMidOrd = 0;   // Порядок значений
 
   // Верхняя Масштабированная линия сетки:
   // var ValMax = Math.ceil(Math.max.apply(null, yPoints));
-  var ValMax = Math.ceil(Math.max.apply(null, pointsAGA.y));
+  var ValMax = Math.ceil(Math.max.apply(null, ptsa.y));
   var ValMaxOrd = 0;
   var ValMaxTmp = ValMax;
   while ( ValMaxTmp > 9) {
     ValMaxOrd++;
-    ValMaxTmp = Math.floor(ValMaxTmp/10);
+    ValMaxTmp = Math.ceil(ValMaxTmp/10);
   }
-  grid.End = ++ValMaxTmp * Math.pow(10,ValMaxOrd);
+  ValMaxTmp *= Math.pow(10,ValMaxOrd);
+  grid.End = ValMaxTmp;
 
   
   // Нижняя Масштабированная линия сетки:
-  var ValMin = Math.floor(Math.min.apply(null, pointsAGA.y));
+  var ValMin = Math.floor(Math.min.apply(null, ptsa.y));
   var ValMinOrd = 0;
   var ValMinTmp = ValMin;
   while ( ValMinTmp > 9) {
@@ -110,13 +119,15 @@ function countGrid() {
     ValMinTmp = Math.floor(ValMinTmp/10);
   }
   ValMinTmp *= Math.pow(10, ValMinOrd);
-  grid.Beg = Math.floor(ValMinTmp/Math.pow(10,ValMaxOrd))*Math.pow(10,ValMaxOrd);
+  grid.Beg = ValMinTmp; // Math.floor(ValMinTmp/Math.pow(10,ValMaxOrd))*Math.pow(10,ValMaxOrd);
   
   // Размах данных:
-  var ValDifOrd = Math.pow(10, ValMinOrd); // Порядок Амплитуды значений.
-  ValMaxTmp = Math.ceil(ValMax/ValDifOrd)*ValDifOrd;
-  ValMinTmp = Math.floor(ValMin/ValDifOrd)*ValDifOrd;
-  console.log('ValMinTmp, ValMaxTmp: ', ValMinTmp, ValMaxTmp);
+  var ValDifOrd; // Порядок Амплитуды значений.
+  // var ValDifOrd = Math.pow(10, ValMinOrd);
+  // ValMaxTmp = Math.ceil(ValMax/ValDifOrd)*ValDifOrd;
+  // ValMaxTmp = accuracy(ValMax, -ValMinOrd, 'ceil');
+  // ValMinTmp = accuracy(ValMin, -ValMinOrd); // Math.floor(ValMin/ValDifOrd)*ValDifOrd;
+  console.log('ValMinTmp, ValMaxTmp, ValMin, ValMax: ', ValMinTmp, ValMaxTmp, ValMin, ValMax);
   var ValDif = ValMaxTmp - ValMinTmp;            // Величина Амплитуды значений.
   var ValDifTmp = ValDif;
   ValDifOrd = 0;
@@ -131,23 +142,68 @@ function countGrid() {
     grid.Beg = ValMinTmp;
     grid.End = ValMaxTmp;
   }
+
   grid.stepY = Math.pow(10, ValDifOrd);
   if ( grid.amplitude/Math.pow(10, ValDifOrd) < 3 ) {
     grid.stepY /=5;
   } else if ( grid.amplitude/Math.pow(10, ValDifOrd) < 5 ) {
     grid.stepY /=2;
   }
+  grid.Beg += grid.stepY * accuracy((ValMin-ValMinTmp)/grid.stepY, 0);
+  console.log('grid.End decreased by: ', grid.stepY * accuracy((ValMaxTmp-ValMax)/grid.stepY, 0));
+  grid.End -= grid.stepY * accuracy((ValMaxTmp-ValMax)/grid.stepY, 0);
 
   // Смещение Сетки снизу нижней границы, если она не нулевая.
   grid.shift = (grid.Beg === 0) ? 0 : ((grid.Beg / Math.pow(10, ValMinOrd))-1)*Math.pow(10, ValMinOrd) + 7*Math.pow(10, ValMinOrd-1); 
 
   grid.scale = YY / (grid.End - grid.shift);
 
-  console.log('grid settings: ', grid);
+  gridScaled.Beg = accuracy((grid.Beg - grid.shift)*grid.scale, accu); // Math.floor((grid.Beg - grid.shift)*grid.scale *accu)/accu;
+  gridScaled.End = accuracy((grid.End - grid.shift)*grid.scale, accu); // Math.floor((grid.End - grid.shift)*grid.scale *accu)/accu;
+  gridScaled.stepY = accuracy(grid.stepY * grid.scale, accu); // Math.floor(grid.stepY * grid.scale *accu)/accu;
+  for (var y = grid.Beg; y <= grid.End; y += grid.stepY) {
+    gridScaled.yVal.push( accuracy((y-grid.shift)*grid.scale, accu) ); // Math.floor((y-grid.shift)*grid.scale *accu)/accu);
+    gridScaled.yLbl.push('' + y);
+  }
+  
+  for (i = 0; i < 10; i++) {
+    gridScaled.xVal.push(i/9*XX);
+    gridScaled.xLbl.push( '' + ptsa.x[findClosestPoint(gridScaled.xVal[i], ptsa.x)] );
+  }
+  
+  return gridScaled;
 
 }
 
+function accuracy(x, acc, method) { 
+  var met = method ? method : 'floor';
+
+  if (met === 'floor') {
+    return Math.floor(x*Math.pow(10, acc))/Math.pow(10, acc);
+  } else if (met === 'ceil') {
+    return Math.ceil(x*Math.pow(10, acc))/Math.pow(10, acc);
+  }
+}
+
+function countScaledChart(ptsa, _accuracy) {
+  var accu = _accuracy ? _accuracy : 4;
+  var ar = {
+    xVal: [],
+    xLbl: [],
+    yVal: [],
+    yLbl: []
+  };
+
+  ar.xVal = ptsa.x.map( x => accuracy(x, accu) );
+  ar.xLbl = ptsa.x.map( x => String( accuracy(x, accu) ) );
+  ar.yVal = ptsa.y.map( y => accuracy( (y-grid.shift)*grid.scale, accu ) );
+  ar.yLbl = ptsa.y.map( y => String( accuracy(y, accu) ) );
+
+  return ar;
+}
+
 function drawGrid(_grid){
+  var i;
   // Фон под отрисовку: 2 строки:
   // ctx.fillStyle = '#18223B';
   // ctx.fillRect(0,0, XX,YY);
@@ -166,31 +222,31 @@ function drawGrid(_grid){
   ctx.closePath();
 
   // Расчитанные линии сетки:
-  for (var y = _grid.Beg; y <= _grid.End; y += _grid.stepY) {
+  var xShift = -0.5*ctxPdngBegX;
+  var xWidth = 0.8*ctxPdngBegX;
+  for (i = 0; i <= _grid.yVal.length - 1; i++) {
     ctx.beginPath();
-    ctx.moveTo(0, (y-grid.shift)*grid.scale);
-    ctx.lineTo(XX, (y-grid.shift)*grid.scale);
+    ctx.moveTo(0, _grid.yVal[i]);
+    ctx.lineTo(XX, _grid.yVal[i]);
     ctx.stroke();
     ctx.closePath();
 
     ctx.scale(1, -1); // >>> Подписи оси Y:
-    ctx.fillText(y, -0.5*ctxPdngBegX, -(y-grid.shift)*grid.scale, 0.8*ctxPdngBegX);
+    ctx.fillText(_grid.yLbl[i], xShift, -_grid.yVal[i], xWidth);
     ctx.scale(1, -1); // <<< Подписи оси Y.
   }
   
   var x, text; // >>> Подписи оси X:
+  xWidth = (1/_grid.xVal.length)*XX;
   ctx.font = 'normal normal 100 10px Arial';
   ctx.scale(1, -1);
-    for (var i = 0; i < 10; i++) {
-      x = i/9*XX;
-      text = pointsSrc[findClosestPoint(x, pointsSrc)];
-      ctx.fillText(text, x, 30, 0.1*XX);
+    for (i = 0; i < _grid.xVal.length; i++) {
+      ctx.fillText(_grid.xLbl[i], _grid.xVal[i], 30, xWidth);
     }
   ctx.scale(1, -1); // <<< Подписи оси X.
 }
 
-var tension = 0.5;
-function drawCurve(ctx, ptsa, tension, isClosed, numOfSegments, showPoints) {
+function drawCurve(ctx, ptsa, _tension, isClosed, numOfSegments, showPoints) {
   grd = ctx.createLinearGradient(0, 0, XX, YY);
   grd.addColorStop(0, '#959959');
   grd.addColorStop(1, '#0296E9');
@@ -199,92 +255,67 @@ function drawCurve(ctx, ptsa, tension, isClosed, numOfSegments, showPoints) {
   showPoints  = showPoints ? showPoints : false;
 
   ctx.beginPath();
-  drawLines(ctx, getCurvePoints(ptsa, tension, isClosed, numOfSegments));
+  drawLines(ctx, getCurvePoints(ptsa, _tension, isClosed, numOfSegments));
 
   ctx.stroke();
   if (showPoints) {
     ctx.beginPath();
-    for(var i = 0; i < ptsa.x.length - 1; i++) {
-      ctx.fillRect(ptsa.x[i] - 1, ptsa.y[i] - 1, 4, 4);
+    for(var i = 0; i < ptsa.xVal.length - 1; i++) {
+      ctx.fillRect(ptsa.xVal[i] - 1, ptsa.yVal[i] - 1, 4, 4);
     }
-    // for(var i = 0; i < ptsa.length - 1; i += 2) {
-    //   ctx.fillRect(ptsa[i] - 2, ptsa[i+1] - 2, 4, 4);
-    // }
     ctx.closePath();
   }
-}
+} // Зависимости: (drawLines, getCurvePoints)
 
-function getCurvePoints(pts, tension, isClosed, numOfSegments) {
+function getCurvePoints(pts, _tension, isClosed, numOfSegments) {
 
     // use input value if provided, or use a default value   
-    tension = (typeof tension != 'undefined') ? tension : 0.5;
+    var __tension = (typeof _tension != 'undefined') ? _tension : 0.5;
     isClosed = isClosed ? isClosed : false;
     numOfSegments = numOfSegments ? numOfSegments : 16;
 
     var _pts = {x:[], y:[]}, res = {x:[], y:[]};    // clone array
-    // var _pts = [], res = [];    // clone array
     var x, y;                      // our x,y coords
     var t1x, t2x, t1y, t2y;        // tension vectors
     var c1, c2, c3, c4;            // cardinal points
     var st, t, i;                  // steps based on num. of segments
 
     // clone array so we don't change the original
-    //
-    // _pts = pts.slice(0);
-    _pts.x = pts.x.slice(0);
-    _pts.y = pts.y.slice(0);
+    _pts.x = pts.xVal.slice(0);
+    _pts.y = pts.yVal.slice(0);
 
     // The algorithm require a previous and next point to the actual point array.
     // Check if we will draw closed or open curve.
     // If closed, copy end points to beginning and first points to end
     // If open, duplicate first points to befinning, end points to end
     if (isClosed) {
-        _pts.x.unshift(pts.x[pts.x.length - 1]); // x
-        _pts.y.unshift(pts.y[pts.y.length - 1]); // y
-        _pts.x.unshift(pts.x[pts.x.length - 1]); // x
-        _pts.y.unshift(pts.y[pts.y.length - 1]); // y
-        _pts.x.push(pts.x[0]);
-        _pts.y.push(pts.y[0]);
+        _pts.x.unshift(pts.xVal[pts.xVal.length - 1]);
+        _pts.y.unshift(pts.yVal[pts.yVal.length - 1]);
+        _pts.x.unshift(pts.xVal[pts.xVal.length - 1]);
+        _pts.y.unshift(pts.yVal[pts.yVal.length - 1]);
+        _pts.x.push(pts.xVal[0]);
+        _pts.y.push(pts.yVal[0]);
     }
     else {
-        _pts.x.unshift(pts.x[0]);   //copy 1. point and insert at beginning
-        _pts.y.unshift(pts.y[0]);
-        _pts.x.push(pts.x[pts.x.length - 1]); //copy last point and append
-        _pts.y.push(pts.y[pts.y.length - 1]);
+        _pts.x.unshift(pts.xVal[0]);   //copy 1. point and insert at beginning
+        _pts.y.unshift(pts.yVal[0]);
+        _pts.x.push(pts.xVal[pts.xVal.length - 1]); //copy last point and append
+        _pts.y.push(pts.yVal[pts.yVal.length - 1]);
     }
-    // if (isClosed) {
-    //     _pts.unshift(pts[pts.length - 1]);
-    //     _pts.unshift(pts[pts.length - 2]);
-    //     _pts.unshift(pts[pts.length - 1]);
-    //     _pts.unshift(pts[pts.length - 2]);
-    //     _pts.push(pts[0]);
-    //     _pts.push(pts[1]);
-    // }
-    // else {
-    //     _pts.unshift(pts[1]);   //copy 1. point and insert at beginning
-    //     _pts.unshift(pts[0]);
-    //     _pts.push(pts[pts.length - 2]); //copy last point and append
-    //     _pts.push(pts[pts.length - 1]);
-    // }
 
     // ok, lets start..
 
     // 1. loop goes through point array
     // 2. loop goes through each segment between the 2 pts + 1e point before and after
-    // for (i=2; i < (_pts.length - 4); i+=2) {
     for (i=1; i < (_pts.x.length - 2); i++) {
         for (t=0; t <= numOfSegments; t++) {
 
             // calc tension vectors
-            // t1x = (_pts[i+2] - _pts[i-2]) * tension;
-            // t2x = (_pts[i+4] - _pts[i]) * tension;
-            t1x = (_pts.x[i+1] - _pts.x[i-1]) * tension;
-            t2x = (_pts.x[i+2] - _pts.x[i]) * tension;
+            t1x = (_pts.x[i+1] - _pts.x[i-1]) * __tension;
+            t2x = (_pts.x[i+2] - _pts.x[i]) * __tension;
 
-            // t1y = (_pts[i+3] - _pts[i-1]) * tension;
-            // t2y = (_pts[i+5] - _pts[i+1]) * tension;
-            t1y = (_pts.y[i+1] - _pts.y[i-1]) * tension;
-            t2y = (_pts.y[i+2] - _pts.y[i]) * tension;
+            t1y = (_pts.y[i+1] - _pts.y[i-1]) * __tension;
+            t2y = (_pts.y[i+2] - _pts.y[i]) * __tension;
 
             // calc step
             st = t / numOfSegments;
@@ -296,8 +327,6 @@ function getCurvePoints(pts, tension, isClosed, numOfSegments) {
             c4 =       Math.pow(st, 3)  -     Math.pow(st, 2);
 
             // calc x and y cords with common control vectors
-            // x = c1 * _pts[i]    + c2 * _pts[i+2] + c3 * t1x + c4 * t2x;
-            // y = c1 * _pts[i+1]  + c2 * _pts[i+3] + c3 * t1y + c4 * t2y;
             x = c1 * _pts.x[i]     + c2 * _pts.x[i+1] + c3 * t1x + c4 * t2x;
             y = c1 * _pts.y[i]     + c2 * _pts.y[i+1] + c3 * t1y + c4 * t2y;
 
@@ -312,37 +341,35 @@ function getCurvePoints(pts, tension, isClosed, numOfSegments) {
 }
 
 function drawLines(ctx, pts) {
-    // ctx.moveTo(pts[0], (pts[1]-grid.shift)*grid.scale);
-    // for(var i=2; i<pts.length-1; i+=2) ctx.lineTo(pts[i], (pts[i+1]-grid.shift)*grid.scale);
-    ctx.moveTo(pts.x[0], (pts.y[0]-grid.shift)*grid.scale);
+    ctx.moveTo(pts.x[0], pts.y[0]);
     for ( var i=1; i<pts.x.length-1; i++) {
-      ctx.lineTo(pts.x[i], (pts.y[i]-grid.shift)*grid.scale);
+      ctx.lineTo(pts.x[i], pts.y[i]);
     }
 }
 
 // Найти ближайшую правую точку x:
 // Формат массива: [x0,y0, x1,y1, ... xN,yN]
 function findClosestPoint(x, arPoints, _left, _right) {
-  var points = arPoints.length > 0 ? arPoints : points;
+  var points = arPoints.length > 0 ? arPoints : pointsWork.xVal;
   var left = _left ? _left : 0;
   // var right = _right ? _right : qtyPoints*2;
-  var right = _right ? _right : arPoints.length-2;
+  var right = _right ? _right : points.length-1;
   var middle;
   var k;
 
   while (left <= right) {
     middle = Math.ceil((left+right)/2);
-    middle = Math.ceil(middle/2)*2; // Делаем число четным "вправо".
+    // middle = Math.ceil(middle/2)*2; // Делаем число четным "вправо".
     if ( points[middle] === x ) {
       break; // Остановиться с результатом x.
     } else if ( points[middle] > x ) {
-      right = middle - 2;
+      right = middle - 1;
     } else if ( points[middle] < x ) {
-      left = middle + 2;
+      left = middle + 1;
     }
   }
 
-  if ( Math.abs(x - points[0]) < Math.abs(x - points[2]) ) {
+  if ( Math.abs(x - points[0]) < Math.abs(x - points[1]) ) {
     k = 0; // Костыль для отрисовки нулевой точки.
   } else {
     k = Math.max(left, middle); // Отрисовка ближайшей правой точки.
@@ -351,22 +378,22 @@ function findClosestPoint(x, arPoints, _left, _right) {
   return k;
 }
 
-function drawVertical(x_) {
+function drawVertical(ptsa, _x) {
   const y1 = -40;       // Отступ первой выноски вниз от области рисования графика.
   const widthVert = 3; // Толщина вертикальной линии с выносками.
-  var x = x_;
+  var x = _x;
   var y2; // Высота второй выноски - на графике.
-  var k = findClosestPoint(x, pointsSrc);
+  var k = findClosestPoint(x, ptsa.xVal);
   
-  x = pointsSrc[k];
-  y2 = pointsSrc[k+1];
-  var calloutText1 = x;
-  var calloutText2 = y2;
+  x = ptsa.xVal[k];
+  y2 = ptsa.yVal[k];
+  var calloutText1 = ptsa.xLbl[k];
+  var calloutText2 = ptsa.yLbl[k];
 
   ctx.fillStyle = '#4A90E2';
   ctx.fillRect(x - widthVert/2, y1, widthVert, YY-y1);
   drawCallout(x, y1, widthVert, calloutText1, '', '12px Arial', '#4A90E2', '#18223B', 20,8,4, false);
-  drawCallout(x, (y2-grid.shift)*grid.scale, widthVert, calloutText2, '', '12px Arial', '#3CC182', '#3CC182', 20,8,4, true);
+  drawCallout(x, y2, widthVert, calloutText2, '', '12px Arial', '#3CC182', '#3CC182', 20,8,4, true);
 }
 
 function drawCallout(x, y, rBullet, text1, text2, font, strokeStyle, fillStyle, R_, r_, r2_, toTop_) {
@@ -480,20 +507,20 @@ function drawCallout(x, y, rBullet, text1, text2, font, strokeStyle, fillStyle, 
 }
 
 
+var tension = 0.5;
 initCanvas();
 
 // Make some points:
 // Формат массива: [x0,y0, x1,y1, ... xN,yN]
 var qtyPoints = 90;
-pointsSrc = getDataPoints(qtyPoints).slice(0);
-pointsWork.x = pointsAGA.x.slice(0);
-pointsWork.y = pointsAGA.y.slice(0);
+pointsSrc = getDataPoints(qtyPoints);
 
-countGrid();
-// pointsWork = countGrid();
-drawGrid(grid);
+gridScaled = countScaledGrid(pointsSrc);
+pointsWork = countScaledChart(pointsSrc, 4);
+
+drawGrid(gridScaled);
 drawCurve(ctx, pointsWork, tension, false, false, false);
-drawVertical(0);
+drawVertical(pointsWork, 0);
 
 canvas.addEventListener('mousemove', function (e) {
   var mouseX = e.clientX - canvas.getBoundingClientRect().left - ctxPdngBegX;
@@ -501,9 +528,9 @@ canvas.addEventListener('mousemove', function (e) {
   if ( 0 < mouseX && mouseX < XX && 0 < mouseY && mouseY < YY ) {
 
     initCanvas();
-    drawGrid(grid);
+    drawGrid(gridScaled);
     drawCurve(ctx, pointsWork, tension, false, false, false);
-    drawVertical(mouseX);
+    drawVertical(pointsWork, mouseX);
   }
 });
 
